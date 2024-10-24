ea589cba015e24e831d4f8938affa803a7376475
juratbek <juratbekxudayberganov9@gmail.com>
2024-10-25 00:31:13 +0500

    changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ const api = axios.create({
 
 api.interceptors.request.use(async (config) => {
     const accessToken = localStorage.getItem("accessToken");
-    
+
     config.headers.Authorization = `Bearer ${accessToken}`;
 
     return config;
@@ -18,6 +18,13 @@ api.interceptors.response.use(
     },
     async function (error) {
         console.log(error);
+        if (error.response.status === (401 || 403)) {
+            if (window.location.pathname !== "/auth") {
+                localStorage.clear();
+                window.location.href = "/auth";
+            }
+        }
+
         return Promise.reject(error);
     }
 );
